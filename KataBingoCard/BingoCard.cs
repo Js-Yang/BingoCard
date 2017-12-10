ef4b794de7cf184c1564c04107656acd0fe8bffb
Js-Yang <chyang590@gmail.com>
2017-12-11 01:21:37 +0800

    remove namespace

--- KataBingoCard/BingoCard.cs
+++ KataBingoCard/BingoCard.cs
@@ -1,42 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
-namespace KataBingoCard
+public class BingoCard
 {
-    public class BingoCard
+    private static readonly List<string> Letters = new List<string> { "B", "I", "N", "G", "O" };
+    private static int NextRange = 1;
+
+    public static string[] GetCard()
     {
-        private static readonly List<string> Letters = new List<string> {"B", "I", "N", "G", "O"};
-        private static int NextRange = 1;
-
-        public static string[] GetCard()
+        NextRange = 1;
+        var bingoCard = new List<string>();
+        foreach (var letter in Letters)
         {
-            NextRange = 1;
-            var bingoCard = new List<string>();
-            foreach (var letter in Letters)
+            var level = 5;
+            if (letter == "N")
             {
-                var level = 5;
-                if (letter == "N")
-                {
-                    level--;
-                }
-
-                AddNumbersWith(level, bingoCard, letter);
+                level--;
             }
 
-            return bingoCard.ToArray();
+            AddNumbersWith(level, bingoCard, letter);
         }
 
-        private static void AddNumbersWith(int level, List<string> bingoCard, string letter)
+        return bingoCard.ToArray();
+    }
+
+    private static void AddNumbersWith(int level, List<string> bingoCard, string letter)
+    {
+        for (var i = level * 2; i >= 1; i -= 2)
         {
-            for (var i = level; i >= 1; i--)
-            {
-                var number = i + NextRange;
-                bingoCard.Add(letter + number);
-            }
-            NextRange += 15;
+            var number = i + NextRange;
+            bingoCard.Add(letter + number);
         }
+        NextRange += 15;
     }
 }
