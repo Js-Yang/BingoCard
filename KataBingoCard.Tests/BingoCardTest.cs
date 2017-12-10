bf5ad3a642c9369ea6e2fc73c05e81939348640e
Js-Yang <chyang590@gmail.com>
2017-12-11 01:20:20 +0800

    add test case NumbersWithinColumnAreAllInTheCorrectRange and NumbersWithinColumnAreInRandomOrder

--- KataBingoCard.Tests/BingoCardTest.cs
+++ KataBingoCard.Tests/BingoCardTest.cs
@@ -49,5 +49,40 @@ namespace KataBingoCard.Tests
 
             Assert.IsTrue(Regex.IsMatch(columns, "^[B]*[I]*[N]*[G]*[O]*$"));
         }
+        
+        [TestCase("B", 1, 15)]
+        [TestCase("I", 16, 30)]
+        [TestCase("N", 31, 45)]
+        [TestCase("G", 46, 60)]
+        [TestCase("O", 61, 75)]
+        public void NumbersWithinColumnAreAllInTheCorrectRange(string column, int start, int end)
+        {
+            var numbers = BingoCard.GetCard().Where(x => x.StartsWith(column)).ToList();
+
+            foreach (var number in numbers)
+            {
+                var n = Convert.ToInt32(number.Substring(1));
+                Assert.GreaterOrEqual(n, start, "Column {0} should be in the range between {1} and {2}, found: {3}", column, start, end, number);
+                Assert.LessOrEqual(n, end, "Column {0} should be in the range between {1} and {2}, found: {3}", column, start, end, number);
+            }
+        }
+        
+        [Test]
+        public void NumbersWithinColumnAreInRandomOrder()
+        {
+            var card = BingoCard.GetCard().Select(x => Convert.ToInt32(x.Substring(1))).ToArray();
+
+            var isRandom = false;
+            for (var i = 1; i < card.Length; i++)
+            {
+                if (card[i - 1] > card[i])
+                {
+                    isRandom = true;
+                    break;
+                }
+            }
+
+            Assert.IsTrue(isRandom, "Unlikely result, is the list ordered?");
+        }
     }
 }
--- KataBingoCard/BingoCard.cs
+++ KataBingoCard/BingoCard.cs
@@ -8,10 +8,12 @@ namespace KataBingoCard
 {
     public class BingoCard
     {
-        private static readonly List<string> Letters = new List<string>() {"B", "I", "N", "G", "O"};
+        private static readonly List<string> Letters = new List<string> {"B", "I", "N", "G", "O"};
+        private static int NextRange = 1;
 
         public static string[] GetCard()
         {
+            NextRange = 1;
             var bingoCard = new List<string>();
             foreach (var letter in Letters)
             {
@@ -29,10 +31,12 @@ namespace KataBingoCard
 
         private static void AddNumbersWith(int level, List<string> bingoCard, string letter)
         {
-            for (var i = 0; i < level; i++)
+            for (var i = level; i >= 1; i--)
             {
-                bingoCard.Add(letter + "10");
+                var number = i + NextRange;
+                bingoCard.Add(letter + number);
             }
+            NextRange += 15;
         }
     }
 }
