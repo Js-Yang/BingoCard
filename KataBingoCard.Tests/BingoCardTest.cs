06d59ba89f1ed5eb3f17f9d431cfb950bbfb424b
Js-Yang <chyang590@gmail.com>
2017-12-11 00:38:12 +0800

    add test GetCard_Check_Array_Count

--- KataBingoCard.Tests/BingoCardTest.cs
+++ KataBingoCard.Tests/BingoCardTest.cs
@@ -36,5 +36,15 @@ namespace KataBingoCard.Tests
         {
             Assert.AreEqual(24, BingoCard.GetCard().Length);
         }
+
+        [TestCase("B",5,TestName = "B Count Should be 5")]
+        [TestCase("I",5,TestName = "I Count Should be 5")]
+        [TestCase("N",4,TestName = "N Count Should be 4")]
+        [TestCase("G",5,TestName = "G Count Should be 5")]
+        [TestCase("O",5,TestName = "O Count Should be 5")]
+        public void GetCard_Check_Array_Count(string charactor, int expected)
+        {
+            Assert.AreEqual(expected, BingoCard.GetCard().Count(x => x.Contains(charactor)));
+        }
     }
 }
