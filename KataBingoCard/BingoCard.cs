7c1fd540c12a72f58734b8c841894577dba68c61
Js-Yang <chyang590@gmail.com>
2017-12-12 00:58:53 +0800

    refactor: extract class to seprate responsibility

--- KataBingoCard/BingoCard.cs
+++ KataBingoCard/BingoCard.cs
@@ -3,60 +3,33 @@ using System.Collections.Generic;
 
 public class BingoCard
 {
-    private static readonly List<string> Letters = new List<string> { "B", "I", "N", "G", "O" };
-    private static int BaseRange = 15;
-
     public static string[] GetCard()
     {
         var bingoCard = new List<string>();
-        foreach (var letter in Letters)
-        {
-            bingoCard.AddRange(GetRndNumbersBy(letter));
-        }
-
+        bingoCard.AddRange(RndNumbers.Generate("B", 1, 15, 5));
+        bingoCard.AddRange(RndNumbers.Generate("I", 16, 30, 5));
+        bingoCard.AddRange(RndNumbers.Generate("N", 31, 45, 4));
+        bingoCard.AddRange(RndNumbers.Generate("G", 46, 60, 5));
+        bingoCard.AddRange(RndNumbers.Generate("O", 61, 75, 5));
         return bingoCard.ToArray();
     }
+}
 
-    private static List<string> GetRndNumbersBy(string letter)
+public class RndNumbers
+{
+    public static IEnumerable<string> Generate(string label, int start, int end, int count)
     {
-        var numbersOfLetter = new List<string>();
-        var index = Letters.IndexOf(letter);
-        var numbers = GetNumbersBetween(index * BaseRange, (index + 1) * BaseRange);
-        for (var i = 0; i < GetLevel(letter); i++)
+        var rndNumbers = new List<string>();
+        var rnd = new Random(Guid.NewGuid().GetHashCode());
+        while (rndNumbers.Count < count)
         {
-            numbersOfLetter.Add(letter + RandomTakeFrom(numbers));
-        }
-
-        return numbersOfLetter;
-    }
-
-    private static int GetLevel(string letter)
-    {
-        return letter == "N" ? 4 : 5;
-    }
-
-    private static List<int?> GetNumbersBetween(int start, int end)
-    {
-        var numbers = new List<int?>();
-        for (var i = start; i < end; i++)
-        {
-            numbers.Add(i);
-        }
-        return numbers;
-    }
-
-    private static int? RandomTakeFrom(List<int?> numbers)
-    {
-        while (true)
-        {
-            var rnd = new Random(Guid.NewGuid().GetHashCode());
-            var randPosition = rnd.Next(1, 15);
-            if (numbers[randPosition] != null)
+            var rndNumber = label + rnd.Next(start, end);
+            if (!rndNumbers.Contains(rndNumber))
             {
-                var rndNumber = numbers[randPosition];
-                numbers[randPosition] = null;
-                return rndNumber;
+                rndNumbers.Add(rndNumber);
             }
         }
+
+        return rndNumbers;
     }
 }
